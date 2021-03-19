1e92e014031b2da4db0a2da5fa800a975949f0d5
LevyAM <levyzudo@msn.com>
2021-03-19 00:07:47 -0300

    Desafio concluído

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,13 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user || user.admin === false) {
+      throw new Error("User doesnt exists or user isnt an admin!");
+    }
+
+    return this.usersRepository.list();
   }
 }
 
